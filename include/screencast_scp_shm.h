01fab0306ad78855502b0c7c5dbfd902dc936798
columbarius <co1umbarius@protonmail.com>
2020-09-03 01:57:39 +0200

    Seperate shm frame structs and move them to screencast_scp_shm.h

---
+++ include/screencast_scp_shm.h
@@ -0,0 +1,27 @@
+#ifndef SCREENCAST_SCP_H
+#define SCREENCAST_SCP_H
+
+struct xdpw_frame_scp_shm_damage {
+	uint32_t x;
+	uint32_t y;
+	uint32_t width;
+	uint32_t height;
+};
+
+struct xdpw_frame_scp_shm {
+	uint32_t width;
+	uint32_t height;
+	uint32_t size;
+	uint32_t stride;
+	bool y_invert;
+	uint64_t tv_sec;
+	uint32_t tv_nsec;
+	enum wl_shm_format format;
+	struct xdpw_frame_damage damage;
+	struct wl_buffer *buffer;
+	void *data;
+};
+
+
+
+#endif /* !SCREENCAST_SCP_H */
