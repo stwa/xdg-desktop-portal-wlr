f36f8fd49593ef3eb163cbf330c37ac880562be8
columbarius <co1umbarius@protonmail.com>
2020-09-03 02:45:02 +0200

    Add xdpw_format_pw to screencast_common.h

--- include/screencast_common.h
+++ include/screencast_common.h
@@ -98,6 +98,8 @@ struct xdpw_wlr_output {
 };
 
 void randname(char *buf);
+enum spa_video_format xdpw_format_pw(
+	struct xdpw_screencast_instance *cast);
 enum spa_video_format xdpw_format_pw_strip_alpha(enum spa_video_format format);
 
 #endif /* SCREENCAST_COMMON_H */
