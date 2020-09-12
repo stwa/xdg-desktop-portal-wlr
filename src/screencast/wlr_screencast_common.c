816101aa672b016dd539303184864e2e5ffd07a8
columbarius <co1umbarius@protonmail.com>
2020-09-13 01:18:32 +0200

    Move wl_buffer creation to wlr_screencast_common.c

---
+++ src/screencast/wlr_screencast_common.c
@@ -0,0 +1,77 @@
+#include "wlr_screencast_common.h"
+
+#include "wlr-screencopy-unstable-v1-client-protocol.h"
+#include "xdg-output-unstable-v1-client-protocol.h"
+#include <fcntl.h>
+#include <limits.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/mman.h>
+#include <sys/param.h>
+#include <sys/stat.h>
+#include <sys/wait.h>
+#include <unistd.h>
+#include <wayland-client-protocol.h>
+
+#include "screencast.h"
+#include "pipewire_screencast.h"
+#include "xdpw.h"
+#include "logger.h"
+
+static int anonymous_shm_open(void) {
+	char name[] = "/xdpw-shm-XXXXXX";
+	int retries = 100;
+
+	do {
+		randname(name + strlen(name) - 6);
+
+		--retries;
+		// shm_open guarantees that O_CLOEXEC is set
+		int fd = shm_open(name, O_RDWR | O_CREAT | O_EXCL, S_IRUSR | S_IWUSR);
+		if (fd >= 0) {
+			shm_unlink(name);
+			return fd;
+		}
+	} while (retries > 0 && errno == EEXIST);
+
+	return -1;
+}
+
+struct wl_buffer *wlr_create_shm_buffer(struct xdpw_screencast_instance *cast,
+		enum wl_shm_format fmt, int width, int height, int stride,
+		void **data_out) {
+	struct xdpw_screencast_context *ctx = cast->ctx;
+	int size = stride * height;
+
+	int fd = anonymous_shm_open();
+	if (fd < 0) {
+		logprint(ERROR, "wlroots: shm_open failed");
+		return NULL;
+	}
+
+	int ret;
+	while ((ret = ftruncate(fd, size)) == EINTR);
+
+	if (ret < 0) {
+		close(fd);
+		logprint(ERROR, "wlroots: ftruncate failed");
+		return NULL;
+	}
+
+	void *data = mmap(NULL, size, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
+	if (data == MAP_FAILED) {
+		logprint(ERROR, "wlroots: mmap failed: %m");
+		close(fd);
+		return NULL;
+	}
+
+	struct wl_shm_pool *pool = wl_shm_create_pool(ctx->shm, fd, size);
+	close(fd);
+	struct wl_buffer *buffer =
+		wl_shm_pool_create_buffer(pool, 0, width, height, stride, fmt);
+	wl_shm_pool_destroy(pool);
+
+	*data_out = data;
+	return buffer;
+}
