eae1b0b12ceea87bfc36018f6b79adbd3543b928
columbarius <co1umbarius@protonmail.com>
2021-06-22 18:17:33 +0200

    screencast: add option to force implicit modifier to be linear

--- src/screencast/pipewire_screencast.c
+++ src/screencast/pipewire_screencast.c
@@ -1,10 +1,12 @@
 #include "pipewire_screencast.h"
 
+#include <gbm.h>
 #include <pipewire/pipewire.h>
 #include <spa/utils/result.h>
 #include <spa/param/props.h>
 #include <spa/param/format-utils.h>
 #include <spa/param/video/format-utils.h>
+#include <stdint.h>
 #include <sys/mman.h>
 #include <unistd.h>
 #include <assert.h>
@@ -232,9 +234,15 @@ static void pwr_handle_stream_add_buffer(void *data, struct pw_buffer *buffer) {
 
 		struct xdpw_pwr_screencopy_dmabuf_frame *frame = calloc(1,sizeof(struct xdpw_pwr_screencopy_dmabuf_frame));
 
+		uint32_t flags = GBM_BO_USE_RENDERING;
+
+		if (cast->ctx->state->config->screencast_conf.force_mod_linear)
+			flags |= GBM_BO_USE_LINEAR;
+
 		frame->bo = gbm_bo_create(cast->ctx->gbm,
 				cast->screencopy_dmabuf_frame.width, cast->screencopy_dmabuf_frame.height,
-				cast->screencopy_dmabuf_frame.fourcc, GBM_BO_USE_RENDERING);
+				cast->screencopy_dmabuf_frame.fourcc, flags);
+
 		if (frame->bo == NULL) {
 			logprint(ERROR, "wlroots: failed to create gbm_bo");
 			free(frame);
