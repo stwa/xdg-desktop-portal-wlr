dfd4673c14a7336804c1bea5d3b43e8e258578c4
columbarius <co1umbarius@protonmail.com>
2021-08-08 03:31:49 +0200

    screencast: add build_buffer function
    This function makes it easier to omit attributes like size or stride for
    cases where they won't be well defined like muliplanar dmabufs

--- src/screencast/pipewire_screencast.c
+++ src/screencast/pipewire_screencast.c
@@ -16,6 +16,24 @@
 #include <libdrm/drm_fourcc.h>
 #include "linux-dmabuf-unstable-v1-client-protocol.h"
 
+static struct spa_pod *build_buffer(struct spa_pod_builder *b, uint32_t blocks, uint32_t size, uint32_t stride, uint32_t datatype)
+{
+	assert(blocks > 0);
+	assert(datatype > 0);
+	struct spa_pod_frame f[1];
+
+	spa_pod_builder_push_object(b, &f[0], SPA_TYPE_OBJECT_ParamBuffers, SPA_PARAM_Buffers);
+	spa_pod_builder_add(b, SPA_PARAM_BUFFERS_buffers, SPA_POD_CHOICE_RANGE_Int(XDPW_PWR_BUFFERS, 1, 32), 0);
+	spa_pod_builder_add(b, SPA_PARAM_BUFFERS_blocks, SPA_POD_Int(blocks), 0);
+	if (size > 0)
+		spa_pod_builder_add(b, SPA_PARAM_BUFFERS_size, SPA_POD_Int(size), 0);
+	if (stride > 0)
+		spa_pod_builder_add(b, SPA_PARAM_BUFFERS_stride, SPA_POD_Int(stride), 0);
+	spa_pod_builder_add(b, SPA_PARAM_BUFFERS_align, SPA_POD_Int(XDPW_PWR_ALIGN), 0);
+	spa_pod_builder_add(b, SPA_PARAM_BUFFERS_dataType, SPA_POD_CHOICE_FLAGS_Int(datatype), 0);
+	return spa_pod_builder_pop(b, &f[0]);
+}
+
 static struct spa_pod *build_format(struct spa_pod_builder *b, enum spa_video_format format,
 		uint32_t width, uint32_t height, uint32_t framerate,
 		uint64_t *modifiers, int modifier_count)
