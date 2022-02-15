d0ead92d08b52a04c1149615634035ea9907d38d
columbarius <co1umbarius@protonmail.com>
2022-02-15 12:03:09 +0100

    screencast: add support for format BGR

--- src/screencast/screencast_common.c
+++ src/screencast/screencast_common.c
@@ -73,6 +73,8 @@ enum spa_video_format xdpw_format_pw_from_wl_shm(enum wl_shm_format format) {
 		return SPA_VIDEO_FORMAT_ARGB;
 	case WL_SHM_FORMAT_BGRX8888:
 		return SPA_VIDEO_FORMAT_xRGB;
+	case WL_SHM_FORMAT_BGR888:
+		return SPA_VIDEO_FORMAT_RGB;
 	case WL_SHM_FORMAT_NV12:
 		return SPA_VIDEO_FORMAT_NV12;
 	case WL_SHM_FORMAT_XRGB2101010:
